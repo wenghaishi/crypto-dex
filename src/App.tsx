456a5a44a1204bd5d5b869b5c8f09cc65782223b
weng haishi <74546450+wenghaishi@users.noreply.github.com>
2023-04-17 13:28:31 +0800

    created swap component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Swap from "./components/Swap";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
+    <div className="flex flex-col">
       <Navbar />
+      <Swap />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
